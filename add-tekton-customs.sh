55ca0fc576c1df8a3868ab2e8b74f262125f525f
DiamJK <83073406+DiamJK@users.noreply.github.com>
2021-05-12 16:29:38 +0200

    Update add-tekton-customs.sh

--- add-tekton-customs.sh
+++ add-tekton-customs.sh
@@ -4,11 +4,11 @@ set -e
 
 NS=$1
 
-echo ""
-echo "Installing buildah task from https://hub-preview.tekton.dev/"
-oc apply -f \
- https://raw.githubusercontent.com/tektoncd/catalog/master/task/buildah/0.1/buildah.yaml \
- -n $NS
+# echo ""
+# echo "Installing buildah task from https://hub-preview.tekton.dev/"
+# oc apply -f \
+#  https://raw.githubusercontent.com/tektoncd/catalog/master/task/buildah/0.1/buildah.yaml \
+# -n $NS
 echo "Installing custom tasks"
 oc apply -f tekton/custom-tasks/push-knative-manifest.yaml -n $NS
 oc apply -f tekton/custom-tasks/workspace-cleaner.yaml -n $NS
