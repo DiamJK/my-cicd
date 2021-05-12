442f1ecb0cbb16498377ab6dded167cab297daba
DiamJK <83073406+DiamJK@users.noreply.github.com>
2021-05-12 16:16:55 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,16 +4,15 @@ set -e
 
 # helm dependency build bootstrap
 # helm upgrade --install bootstrap bootstrap
-echo ""
+# echo ""
 # install helms charts that will initialize the environment 
-echo "Installing openshift-pipelines operator"
-helm upgrade --install openshift-pipelines openshift-pipelines
-echo ""
-echo "Installing openshift-serverless" 
-helm upgrade --install openshift-serverless openshift-serverless
+# echo "Installing openshift-pipelines operator"
+# helm upgrade --install openshift-pipelines openshift-pipelines
+# echo ""
+# echo "Installing openshift-serverless" 
+# helm upgrade --install openshift-serverless openshift-serverless
 echo ""
 echo "Creating cicd, development, staging and production namespaces"
 echo "Added cicd system:image-puller role to default sa in development, staging and production namespaces"
 echo "Added view role to default sa in development, staging and production namespaces"
 helm upgrade --install bootstrap-projects bootstrap-projects
-
